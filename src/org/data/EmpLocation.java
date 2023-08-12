b80e1ca593f3e3382c57228ee9f33832e6807d46
AjayIruthayaraj <ajayiruthayaraj@yahoo.com>
2023-08-12 10:38:30 +0530

    first day done

---
+++ src/org/data/EmpLocation.java
@@ -0,0 +1,32 @@
+package org.data;
+
+
+
+public class EmpLocation {
+	public static void main(String[] args) {
+		
+	
+		
+		int age =25;
+		System.out.println("age is" +age);
+		
+		short id=1234;
+		System.out.println("my age is" +id);
+		
+		byte regno=12;
+		System.out.println("pincode" +regno);
+		
+				
+		
+		
+		
+		
+	}
+	
+	
+	
+}  
+
+
+
+
---
+++ src/org/sample/EmployeeDetails.java
@@ -0,0 +1,20 @@
+package org.sample;
+
+public class EmployeeDetails {
+	public void Empname() {
+		System.out.println("employee name is  Ajay");
+	}
+	
+	public void Empid() {
+		System.out.println(" emp id is 1234");
+
+	}
+	public static void main(String[] args) {
+		EmployeeDetails e= new EmployeeDetails();
+		e.Empid();
+		e.Empname();
+	}
+
+		
+}
+	
